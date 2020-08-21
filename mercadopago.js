372497380cfa5dfabc6ea1f5827b218f6ec38361
Julian Skalic <jskalic@santandertecnologia.com.ar>
2020-08-21 18:49:16 -0300

    Add itnegrator id

--- mercadopago.js
+++ mercadopago.js
@@ -2,7 +2,8 @@ const mercadopago = require ('mercadopago');
 const config = require('./config')
 
 mercadopago.configure({
-  access_token: 'APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398'
+  access_token: 'APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398',
+  integrator_id: 'dev_24c65fb163bf11ea96500242ac130004'
 });
 
 const createPreference = async ({img, title, price}) => {
